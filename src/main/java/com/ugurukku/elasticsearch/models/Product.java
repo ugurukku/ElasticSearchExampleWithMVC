7e0d10ca8e123df5e02bbc34375add33f5b7002c
Ugur <ugurkarimli1@gmail.com>
2023-01-09 16:12:06 +0400

    created Product model

---
+++ src/main/java/com/ugurukku/elasticsearch/models/Product.java
@@ -0,0 +1,63 @@
+package com.ugurukku.elasticsearch.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.Date;
+
+@Document(indexName = "products")
+public class Product {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text, name = "name")
+    private String name;
+
+    @Field(type = FieldType.Text, name = "description")
+    private String description;
+
+    @Field(type = FieldType.Double, name = "price")
+    private double price;
+
+    public Product(String id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
